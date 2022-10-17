15fc7bd7bcf39c338b3574d142a520ed12faa2cc
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-10-17 12:49:44 +0300

    Add langArr and language-change-selector in HTML

---
+++ js/lang.js
@@ -0,0 +1,52 @@
+const langArr = {
+  title: {
+    ru: "Обратись к Колесу Фортуны, и получишь ответ",
+    en: "Turn to the Wheel of Fortune and it will give you the answer",
+    ua: "Питаючий у Колеса Фортуни, знаходить вiдповiдь",
+  },
+  faq: {
+    ru: "Как пользоваться",
+    en: "How to use",
+    ua: "Як користуватися",
+  },
+  firstRule: {
+    ru: "Верхнее поле необязательно",
+    en: "Top field is name of your list (not required)",
+    ua: "Верхнє поле необов'язкове",
+  },
+
+  secondRule: {
+    ru: "Нижнее поле - ваш список (обязательно)",
+    en: "Bottom field is your list (required)",
+    ua: "Нижнє поле – ваш список (обов'язково)",
+  },
+
+  thirdRule: {
+    ru: "Список ведётся через запятую",
+    en: "Listing with commas",
+    ua: "Список ведеться через кому",
+  },
+
+  fourthRule: {
+    ru: "Используйте '-' вместо пробела",
+    en: "Use '-' if you need to show a whitespace.",
+    ua: "Використовуйте '-' замiсть пробiла",
+  },
+  lastRule: {
+    ru: "Пробелы недопустимы",
+    en: "Whitespace is unexpected.",
+    ua: "Вільне місце неприпустимо ",
+  },
+};
+
+// title
+// faq
+// firstRule
+// secondRule
+// thirdRule
+// fourthRule
+// lastRule
+// lang-label-1
+// lang-label-2
+// lang-fortune
+// lang-button-newOrder
